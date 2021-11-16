2dc3989f956b1cc194ccaec95c21d0b039af44ec
O-Siman <72103629+O-Siman@users.noreply.github.com>
2021-11-16 11:13:04 -0500

    Fix

---
+++ src/com/columbushs/unit7/Unit7_1IntroArrayLists.java
@@ -0,0 +1,20 @@
+package com.columbushs.unit7;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Unit7_1IntroArrayLists {
+
+    //Arrays are static in size - you cannot change the size after they are created
+
+    //ArrayLists are dynamic in size - you can change the size any time after creation
+
+    //ArrayLists are designed to store object data not primitives(Int,double,boolean,etc)
+
+    //Need import statement.
+
+    // How to call: ArrayList<DataType> variableName;
+    // DataTypes are not primitives but can be wrapper classes: Boolean, Integer, Double, etc.
+
+}
