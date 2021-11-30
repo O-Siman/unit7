df698479007b9ef81c8c0fe9e7a45bd19f0f8267
O-Siman <osiman23376@columbushs.com>
2021-11-30 11:15:05 -0500

    New

--- src/com/columbushs/unit7/Unit7Main.java
+++ src/com/columbushs/unit7/Unit7Main.java
@@ -1,7 +1,20 @@
 package com.columbushs.unit7;
 
 public class Unit7Main {
+
     public static void main(String[] args) {
-        Unit7_1IntroArrayLists introObj = new Unit7_1IntroArrayLists();
+
+        //Unit7_1IntroArrayLists introObj = new Unit7_1IntroArrayLists();
+
+
+        Unit7_2ArrayListMethods arrayListMethods = new Unit7_2ArrayListMethods();
+        //arrayListMethods.arrayMethods();
+
+        Unit7_3ArrayListTraversing arrayListTrav = new Unit7_3ArrayListTraversing();
+        arrayListTrav.arrayListTrav();
+
+
     }
+
+
 }
