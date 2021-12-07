a37bbe0caceee053f887d079e32cfdddc3bcdd0a
O-Siman <osiman23376@columbushs.com>
2021-12-07 11:07:51 -0500

    New

--- src/com/columbushs/unit7/Unit7Main.java
+++ src/com/columbushs/unit7/Unit7Main.java
@@ -1,10 +1,11 @@
 package com.columbushs.unit7;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class Unit7Main {
 
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
 
         //Unit7_1IntroArrayLists introObj = new Unit7_1IntroArrayLists();
 
@@ -15,7 +16,7 @@ public class Unit7Main {
         Unit7_3ArrayListTraversing arrayListTrav = new Unit7_3ArrayListTraversing();
         //arrayListTrav.arrayListTrav();
 
-        ArrayList<Integer> arrayListInts = new ArrayList<>();
+        ArrayList<Integer> arrayListInts = new ArrayList<Integer>();
         arrayListInts.add(10);
         arrayListInts.add(45);
         arrayListInts.add(3);
@@ -23,7 +24,7 @@ public class Unit7Main {
         arrayListInts.add(567);
         arrayListInts.add(345);
 
-        ArrayList<String> arrayListStr = new ArrayList<>();
+        ArrayList<String> arrayListStr = new ArrayList<String>();
         arrayListStr.add("Apple");
         arrayListStr.add("Orange");
         arrayListStr.add("Banana");
@@ -32,12 +33,39 @@ public class Unit7Main {
         arrayListStr.add("Berry");
 
         Unit7_5Searching searching = new Unit7_5Searching();
-        System.out.println(searching.whereIsTheNumber(3, arrayListInts));
+        //System.out.println(searching.whereIsTheNumber(3, arrayListInts));
 
-        System.out.println(searching.whereIsTheString("Grapes", arrayListStr));
+        //System.out.println(searching.whereIsTheString("Tom", arrayListStr));
+
+        Unit7_6Sorting sorting = new Unit7_6Sorting();
+        //sorting.selectionSort(arrayListInts);
+
+        //sorting.insertionSort(arrayListStr);
 
 
-    }
+        ArrayList<String> Class = new ArrayList<String>();
+        Class.add("Alfonso, Gustavo");
+        Class.add("Andretta, Jan");
+        Class.add("Arisso, Francisco");
+        Class.add("Callejas, Roberto");
+        Class.add("Clark, Michael");
+        Class.add("Cunat, Javier");
+        Class.add("Dickens, Matthew");
+        Class.add("Fernandez, Alessandro");
+        Class.add("Ferrer, Christopher");
+        Class.add("Gonzalez, Erick");
+        Class.add("Gonzalez, Samuel");
+        Class.add("Kakouris, George");
+        Class.add("Martinez, Sebastian");
+        Class.add("Quintana, Andrew");
+        Class.add("Sedano, Nicolas");
+        Class.add("Siman, Omar");
+        Class.add("Solorzano, Felipe");
+        Class.add("Tonanez, Lucas");
+
+        sorting.randomTeams(Class);
+
+    }*/
 
 
 }
---
+++ src/com/columbushs/unit7/Unit7_6Sorting.java
@@ -0,0 +1,61 @@
+package com.columbushs.unit7;
+
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Unit7_6Sorting {
+
+    public void selectionSort(ArrayList<Integer> arrLis) {
+        for (int outerLoop = 0; outerLoop < arrLis.size(); outerLoop++) {
+            int minIndex = outerLoop;
+            for (int innerLoop = outerLoop + 1; innerLoop < arrLis.size(); innerLoop++) {
+                if (arrLis.get(innerLoop).compareTo(arrLis.get(outerLoop)) < 0) {
+                    minIndex = innerLoop;
+                }
+            }
+
+            if (minIndex != outerLoop) {
+                int holder = arrLis.get(minIndex);
+                arrLis.set(minIndex, arrLis.get(outerLoop));
+                arrLis.set(outerLoop, holder);
+            }
+
+        }
+        for (Integer ints : arrLis) {
+            System.out.println(ints);
+        }
+
+    }
+
+    public void insertionSort(ArrayList<String> arrLis) {
+        for (int outerLoop = 1; outerLoop < arrLis.size(); outerLoop++) {
+            String tester = arrLis.get(outerLoop);
+
+            int innerLoop = outerLoop - 1;
+            while (innerLoop >= 0 && tester.compareTo(arrLis.get(innerLoop)) < 0) {
+                arrLis.set(innerLoop + 1, arrLis.get(innerLoop));
+                innerLoop--;
+            }
+            arrLis.set(innerLoop + 1, tester);
+        }
+
+        for (String words : arrLis) {
+            System.out.println(words);
+        }
+
+    }
+
+
+    public void randomTeams(ArrayList<String> arrLis) {
+
+        Collections.shuffle(arrLis);
+        for (int index = 0; index < arrLis.size(); index += 4) {
+            arrLis.add(index, "//////////////////////");
+        }
+        for (String names : arrLis) {
+            System.out.println(names);
+        }
+    }
+
+
+}
